a267e37d84fd48b6dd346637aacc5b0457e41f6a
berkanmen <berkantumut@hotmail.com>
2023-12-05 21:49:49 +0300

    google drive connected

--- app/src/main/java/com/example/start2/home/screens/HomeScreen.kt
+++ app/src/main/java/com/example/start2/home/screens/HomeScreen.kt
@@ -3,40 +3,202 @@ package com.example.start2.home.screens
 
 
 import android.annotation.SuppressLint
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.documentfile.provider.DocumentFile
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 
 
 @Composable
 fun HomeScreen(
     showDetail: () -> Unit
 ) {
-    Scaffold {
-        innerPadding ->
-        Box(modifier = Modifier.fillMaxSize().padding(innerPadding) ) {
-            Text(
-                modifier = Modifier.align(Alignment.Center),
-                text = "Home Screen", style = MaterialTheme.typography.headlineMedium
-            )
+
+    var songName by remember { mutableStateOf("") }
+    var username by remember { mutableStateOf("") }
+    var length by remember { mutableStateOf("") }
+    var tempo by remember { mutableStateOf("") }
+    var recordingType by remember { mutableStateOf("") }
+    var listens by remember { mutableStateOf("") }
+    var releaseYear by remember { mutableStateOf("") }
+    var addedTimestamp by remember { mutableStateOf("") }
+    var selectedFileUri by remember { mutableStateOf<Uri?>(null) }
+
+    val filePickerLauncher = rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        // Handle the result of the file picker here
+        uri?.let {
+            selectedFileUri = it
         }
     }
-}
 
-@Composable
-@Preview
-fun HomeScreenPreview() {
-    HomeScreen(
-        showDetail = {
-            // Handle the action when showing detail
+    // Set the background color
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF5907E9)) // Dark background color
+            .padding(16.dp)
+    )  {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
+                    .padding(16.dp)
+            ) {
+                // Song Name and Username
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    OutlinedTextField(
+                        value = songName,
+                        onValueChange = { songName = it },
+                        label = { Text("Song Name", color = Color.White) },
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    OutlinedTextField(
+                        value = username,
+                        onValueChange = { username = it },
+                        label = { Text("Username", color = Color.White) },
+                        modifier = Modifier.weight(1f)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Other Text Fields...
+                OutlinedTextField(
+                    value = length,
+                    onValueChange = { length = it },
+                    label = { Text("Length (HH:MM:SS)", color = Color.White) },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                OutlinedTextField(
+                    value = tempo,
+                    onValueChange = { tempo = it },
+                    label = { Text("Tempo", color = Color.White) },
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    OutlinedTextField(
+                        value = recordingType,
+                        onValueChange = { recordingType = it },
+                        label = { Text("Recording Type", color = Color.White) },
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    OutlinedTextField(
+                        value = listens,
+                        onValueChange = { listens = it },
+                        label = { Text("Listens", color = Color.White) },
+                        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                        modifier = Modifier.weight(1f)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                OutlinedTextField(
+                    value = releaseYear,
+                    onValueChange = { releaseYear = it },
+                    label = { Text("Release Year (YYYY-MM-DD)", color = Color.White) },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                OutlinedTextField(
+                    value = addedTimestamp,
+                    onValueChange = { addedTimestamp = it },
+                    label = { Text("Added Timestamp (HH:MM:SS)", color = Color.White) },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // File Upload Section
+                Button(
+                    onClick = {
+                        filePickerLauncher.launch("audio/*")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                ) {
+                    Text("Select File", color = Color.White)
+                }
+
+                // Display selected file name
+                selectedFileUri?.let { uri ->
+                    val file = DocumentFile.fromSingleUri(LocalContext.current, uri)
+                    val fileName = file?.name ?: "Unknown File"
+                    Text("Selected File: $fileName", color = Color.White)
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Submit Button
+                Button(
+                    onClick = {
+                        // Form submit action
+                        // TODO: Handle the form submission logic here
+                        // For example, you can create a Music object with the entered data
+                        // and perform any necessary operations like saving to a database.
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                ) {
+                    Text("Add Music", color = Color.White)
+                }
+            }
         }
-    )
-}
+    }
+
